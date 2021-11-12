d02c2c27683cc06c3cb4685be9e08add5f5c6d13
swade19 <Sarah.Wade@bglgroup.co.uk>
2021-11-12 14:37:40 +0000

    first commit

---
+++ HelloMvc/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace HelloMvc.Models
+{
+    public class Customer
+    {
+
+        public string Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage ="Your string is too long")]
+        [DisplayName("Enter you name")]
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+    }
+}
